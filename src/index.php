490b16b876fc368875b00fccbe4e3ef97f69e9f7
Codi3go <110266205+Codi3go@users.noreply.github.com>
2023-12-13 17:42:13 -0500

    Nueva estructura header, footer e informacion

---
+++ src/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>ClubAngeles</title>
+        <link rel="stylesheet" href="./css/estilos.css">
+        <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon.png">
+    </head>
+
+    <body>
+        <?php require_once './header/header.html'?>
+    <!--          Contenedor central                 -->
+        <div class="contenedor">
+        <img src="./img/logoAngeles.png" alt="Ángeles" class="logo_angeles"> 
+        <p class="parrafo_cont"><span >
+
+        En nuestra comunidad, no solo compartimos una pasión por el baloncesto, sino que también nos unimos bajo la inspiradora ala de los Ángeles. En este espacio dedicado al deporte, la camaradería y el espíritu competitivo, te damos la bienvenida con los brazos abiertos.<br>
+        <br>
+        En el Club Ángeles, no importa si eres un jugador experimentado, un apasionado seguidor del baloncesto o simplemente estás interesado en unirte a una familia que celebra la excelencia deportiva. Nuestro compromiso va más allá de la cancha; nos esforzamos por fomentar el desarrollo personal, la amistad y el respeto mutuo.<br>
+        <br>
+        Explora nuestras instalaciones, únete a nuestros eventos emocionantes y sé parte de una experiencia donde el baloncesto se fusiona con la magia de los Ángeles. Estamos emocionados de tenerte aquí y ansiosos por compartir momentos inolvidables tanto dentro como fuera del tabloncillo.<br>
+        <br>
+        ¡Prepárate para elevar tu juego y descubrir la verdadera grandeza en el Club Deportivo de Baloncesto Ángeles!
+        </span></p>
+        </div>
+
+    <?php require_once './footer/footer.html'?>
+    
+    <video src="./img/fondo.mp4" autoplay loop muted class="video"></video>
+    <!--
+        autoplay=reproducción automatica
+        loop=repetir
+        muted= video en silencio-->
+</body>
+
+
+</html>
---
+++ src/informacion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Información</title>
+        <link rel="stylesheet" href="./css/estilos.css">
+        <link rel="stylesheet" href="./css/parallax.css">
+        <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon.png">
+    </head>
+    <body>
+        <?php require_once './header/header.html' ?>
+        
+        <div class="fondo_p">
+            <h1>Información</h1>
+        </div>
+        <section class="seccion_f">
+            <h1 class="titulos">Ejemplo 1</h1>
+            <p class="parrafos">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut assumenda cumque aspernatur repellat quibusdam doloribus est ullam veniam, quos ex obcaecati quas atque nulla consequatur non aliquam adipisci voluptates? Dolore! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, sed quidem. Laborum cupiditate et ipsa officiis alias? Ex repudiandae rem, provident asperiores quidem maiores suscipit, excepturi sit ea, quaerat ipsum!</p>
+        </section>
+        <section class="seccion">
+            <h2>Diseño</h2>
+        </section>
+
+        <?php require_once './footer/footer.html'?>
+
+    </body>
+
+
+</html>
